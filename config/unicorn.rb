27754b4ccd0d3ad2f82652cdf5f5db571ff07a74
0910 <e@nuevediez.com.ar>
2016-02-09 16:07:17 -0300

    deploy configuration

---
+++ config/unicorn.rb
@@ -0,0 +1,19 @@
+# set path to application
+app_root = "/home/ubuntu/applications/festivals/current"
+shared_path = "/home/ubuntu/applications/festivals/shared"
+working_directory app_root
+
+# Logging
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
+
+# Set up socket location
+listen "/tmp/unicorn.festivals.sock", :backlog => 64
+
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+
+# Set master PID location
+pid "#{app_root}/shared/pids/unicorn.festivals.pid"

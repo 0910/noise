73e7e8c94f88511f84bfffa37b2c4c378918a631
0910 <e@nuevediez.com.ar>
2016-03-07 02:53:50 -0300

    Splashes paperclip error resolved

--- app/models/splash.rb
+++ app/models/splash.rb
@@ -4,10 +4,10 @@ class Splash < ActiveRecord::Base
 
   active_admin_translates :title, :subtitle, :call_to_action
 
-  has_attached_file :image, :styles => { :thumb => "120x80>", :small => '480x320>', :medium => '720x480>', :large => '1080x720>' },
+  has_attached_file :photo, :styles => { :thumb => "120x80>", :small => '480x320>', :medium => '720x480>', :large => '1080x720>' },
     :url  => "/assets/splashes/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/splashes/:id/:style/:basename.:extension"
             
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
 end
